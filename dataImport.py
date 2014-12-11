0feff9747d518f2ece6955d438b4239978c93c33
Brenhin Keller <cbkeller@princeton.edu>
2014-12-11 16:51:39 -0500

    Additions to import user interface

--- dataImport.py
+++ dataImport.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Dataset(object):
-    def __init__(self, data, error=None, variableNames=None, independentVariable=None):
+    def __init__(self, data=None, error=None, variableNames=None, independentVariable=None):
         self.data=data   # Numpy array containing full dataset
         self.error=error # Numpy array containing errors for each data point (optional)
         self.variableNames=variableNames # Dictonary containing variable names (optional)
@@ -9,31 +9,59 @@ class Dataset(object):
 
 
 def main():
-    s=raw_input('Select file type to input:\n 1. Delimited ASCII (e.g. .csv)\n')
+    # Create dataset object
+    d=Dataset()
+
+    # Import data
+    while True:
+        s=raw_input('Select:\n 1. Import data array\n 2. Import uncertainty array (assumed 2-sigma, same dimensions as data array) \n 3. Import variable names\n 4. Assign independent variable\n 0. Done importing\n')
+        
+        if s=='0' or s=='': 
+            break 
+        elif s=='1':
+            d.data=getArray()
+        elif s=='2':
+            d.error=getArray()
+        elif s=='3':
+            d.variableNames=None # To be implemented
+        elif s=='4':
+            # Assign independent Variable
+            n=None
+            s=raw_input('Enter column number of independent variable (zero-indexed)\n')
+            try: n=int(s)
+            except: print('Input not an integer')
+            d.independentVariable=n
+
+    return d
+
+
+
+def getArray():
+    # Import numeric array
+    s=raw_input('Select file type to import numeric array:\n 1. Delimited ASCII (e.g. .csv)\n')
     if s=='1' or s=='': # default
-        return delimited()
+        data=delimited()
     else:
         print('Input not recognized\n')
-        return 0 
+        return None
+
+    # Store variables in columns by default
+    variablesAsColumns=raw_input('Are variables stored as rows (0) or columns (1, default)?\n')
+    if variablesAsColumns=='0':
+        data=data.transpose()
+    elif not (variablesAsColumns=='1' or variablesAsColumns==''):
+        print('Input not recognized\n')
+    return data
+
 
 
 def delimited():
     filename=raw_input('Name of delimited file?\n')
     delim=raw_input('Delimiter?\n')
+    data=None
+    try: data=np.genfromtxt(filename, delimiter=delim, dtype=float)
+    except: print('%s not found\n', filename)
+    return data
 
-    data=np.genfromtxt(filename, delimiter=delim, dtype=float)
 
-    # Store variables in columns by default
-    while True:
-        variablesAsColumns=raw_input('Are variables stored as rows (0) or columns (1, default)?\n')
-        if variablesAsColumns=='1' or variablesAsColumns=='':
-            break
-        elif variablesAsColumns=='0':
-            data=data.transpose()
-            break
-        else:
-            print('Input not recognized\n')
-          
-    
-    return Dataset(data)
 
--- hdstats-framework.py
+++ hdstats-framework.py
@@ -4,15 +4,16 @@ import dataAnalysis
 
 
 while True:
-    s=raw_input('Select task:\n 1. Import\n 2. Data cleaning\n 3. Analysis\n 4. Exit\n')
-    if s=='1':
+    s=raw_input('Select task:\n 1. Import\n 2. Data cleaning\n 3. Analysis\n 0. Exit\n')
+    if s=='0':
+        break
+    elif s=='1':
         dataset=dataImport.main()
     elif s=='2':
         dataset=dataCleaning.main(dataset)
     elif s=='3':
         dataset=dataAnalysis.main(dataset)
-    elif s=='4':
-        break
+
     else:
         print('Input not recognized')
 
