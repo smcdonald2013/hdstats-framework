9b4b2714ea1b73a2e52f697aca8b33654fcfb874
Stephen McDonald <smcdonald2013@gmail.com>
2014-12-10 18:04:28 -0800

    Updated test

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import ols
+import lasso
 
 indep = [0, 1, 2]
 dep = [[0, 0], [1, 1], [2, 2]]
@@ -9,6 +10,18 @@ x.fit_model()
 
 print x.fitted_model
 
+print x.regObj.coef_
+
 x.checks()
 
 #print x.acCheck
+
+y = lasso.LASSO(indep,dep)
+
+y.fit_model()
+
+print y.fitted_model
+
+print y.regObj.coef_
+
+y.checks()
