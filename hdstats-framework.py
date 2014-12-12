3cf92d653ca936e29e946aef639564014d61a66e
Brenhin Keller <cbkeller@princeton.edu>
2014-12-11 20:41:22 -0500

    Added access to interactive python interpreter

--- hdstats-framework.py
+++ hdstats-framework.py
@@ -15,7 +15,7 @@ dataset=Dataset()
 
 
 while True:
-    s=raw_input('Select task:\n 1. Import\n 2. Data cleaning\n 3. Analysis\n 0. Exit\n')
+    s=raw_input('Select task:\n 1. Import\n 2. Data cleaning\n 3. Analysis\n 4. Enter Python interpreter\n 0. Exit\n')
     if s=='0':
         break
     elif s=='1':
@@ -24,7 +24,14 @@ while True:
         dataset=dataCleaning.main(dataset)
     elif s=='3':
         dataset=dataAnalysis.main(dataset)
-
+    elif s=='4':
+        print('Type 0 to exit interpreter\n\n')
+        while s!='0':
+            s=raw_input('In: ')
+            if '=' in s:
+                exec(s)
+            else:
+                print(eval(s))
     else:
         print('Input not recognized')
 
