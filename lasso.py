e69b1fc489bd27b62bf083eb9d19a1f5a9aba289
Stephen McDonald <smcdonald2013@gmail.com>
2014-12-20 17:27:47 -0800

    Updated with expanded checks.

--- lasso.py
+++ lasso.py
@@ -1,8 +1,9 @@
 from sklearn import linear_model
 import statsmodels.api as sm
+import checks as c
 
 class LASSO:
-    #Implements ridge regression, with assumption checks
+    #Implements lasso regression, with assumption checks
 
     def __init__(self, indepVar, depVar,alpha=1):
         self.dependentVar = depVar
@@ -10,26 +11,30 @@ class LASSO:
         self.regObj = linear_model.Lasso(alpha=alpha, copy_X=False)
 
     def fit_model(self):
-        self.fitted_model = self.regObj.fit(self.dependentVar, self.independentVar)
-        self.residuals = self.independentVar - self.regObj.decision_function(self.dependentVar)
+        self.fitted_model = self.regObj.fit(self.independentVar, self.dependentVar)
+        self.residuals = self.dependentVar - self.regObj.decision_function(self.independentVar)
 
     def lasso_CV(self):
+        #Performs cross-validation to select the optimal alpha for lasso regression
         self.regObj = linear_model.LassoCV()
         self.fit_model()
         self.regObj = linear_model.Lasso(alpha=self.regObj.alpha_, copy_X=False)
 
     def lasso_lars_CV(self):
+        #Cross-validation using LARS algorithm. Should be used when multicollinearity isn't a problem, and p >> n
         self.regObj = linear_model.LassoLarsCV()
         self.fit_model()
         self.regObj = linear_model.Lasso(alpha=self.regObj.alpha_, copy_X=False)
 
-    def vif(self):
-        #This doesn't currently work, it appears vif may not yet be implemented in stats models
-        self.vif = []
-        for i in range(len(self.dependentVar)):
-            self.vif.append(sm.stats.outliers_influence.variance_inflation.factor(self.dependentVar,i)) 
-
     def checks(self):
-        self.acCheck = sm.stats.stattools.durbin_watson(self.residuals)
-        self.normCheck = sm.stats.diagnostic.normal_ad(self.residuals)
-        #self.mcCheck = self.vif()
+        #Variety of checks for lasso fit
+        self.acCheck = c.acCheck(self.residuals)
+        self.acCheck.check()
+        self.linCheck = c.linCheck(self.independentVar, self.dependentVar)
+        self.linCheck.check()
+        self.normCheck = c.normCheck(self.residuals)
+        self.normCheck.check()
+        self.mcCheck = c.mcCheck(self.independentVar, self.dependentVar, self.residuals)
+        self.mcCheck.check()
+        self.homoskeCheck = c.homoskeCheck(self.residuals, self.independentVar)
+        self.homoskeCheck.check()
