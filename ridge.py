c6e9aba2d5ee9be1ab81bce96911f5c345a85eb0
Stephen McDonald <smcdonald2013@gmail.com>
2014-12-20 17:27:51 -0800

    Updated with expanded checks.

--- ridge.py
+++ ridge.py
@@ -4,27 +4,30 @@ import statsmodels.api as sm
 class RIDGE:
     #Implements ridge regression, with assumption checks
 
-    def __init__(self, indepVar, depVar,alpha):
+    def __init__(self, indepVar, depVar,alpha=1):
         self.dependentVar = depVar
         self.independentVar = indepVar
         self.regObj = linear_model.Ridge(alpha=alpha, copy_X=False)
 
     def fit_model(self):
-        self.fitted_model = self.regObj.fit(self.dependentVar, self.independentVar)
-        self.residuals = self.independentVar - self.regObj.decision_function(self.dependentVar)
+        self.fitted_model = self.regObj.fit(self.independentVar, self.dependentVar)
+        self.residuals = self.dependentVar - self.regObj.decision_function(self.independentVar)
 
     def ridge_CV(self):
+        #Fits ridge using CV
         self.regObj = linear_model.RidgeCV()
         self.fit_model()
         self.regObj = linear_model.Ridge(alpha=self.regObj.alpha_, copy_X=False)
 
-    def vif(self):
-        #This doesn't currently work, it appears vif may not yet be implemented in stats models
-        self.vif = []
-        for i in range(len(self.dependentVar)):
-            self.vif.append(sm.stats.outliers_influence.variance_inflation.factor(self.dependentVar,i)) 
-
-    def checks(self):
-        self.acCheck = sm.stats.stattools.durbin_watson(self.residuals)
-        self.normCheck = sm.stats.diagnostic.normal_ad(self.residuals)
-        #self.mcCheck = self.vif()
+def checks(self):
+        #Variety of checks for ridge fit
+        self.acCheck = c.acCheck(self.residuals)
+        self.acCheck.check()
+        self.linCheck = c.linCheck(self.independentVar, self.dependentVar)
+        self.linCheck.check()
+        self.normCheck = c.normCheck(self.residuals)
+        self.normCheck.check()
+        self.mcCheck = c.mcCheck(self.independentVar, self.dependentVar, self.residuals)
+        self.mcCheck.check()
+        self.homoskeCheck = c.homoskeCheck(self.residuals, self.independentVar)
+        self.homoskeCheck.check()
