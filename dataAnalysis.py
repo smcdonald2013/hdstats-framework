5403ad6d33472f56d36e0fd0bcf0c00f48da97cf
Stephen McDonald <smcdonald2013@gmail.com>
2014-12-20 17:28:28 -0800

    Updated with expanded checks.

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -1,15 +1,40 @@
 import sklearn # scikit-learn
 import numpy as np
+import ols
+import sgd
 
 def main(dataset):
-    s=raw_input('Select data analysis task:\n 1. Print data to screen (test)\n')
-    if s=='1' or s=='': # default
-        np.set_printoptions(precision=3, suppress=True)
-        print(dataset.data)
-        return dataset
-    else:
-        print('Input not recognized')
-        return dataset
+
+    while True:
+        s=raw_input('Select data analysis task:\n 1. Print data to screen\n 2. Regression\n 0. Exit\n')
+        if s=='0':
+            break
+        elif s=='1':
+            np.set_printoptions(precision=3, suppress=True)
+            print(dataset.data)
+            #return dataset
+        elif s=='2':
+            if dataset.data.shape[0] < 100000:
+                print('Running OLS Regression')
+                reg = ols.OLS(dataset.data[:,1:dataset.data.shape[1]],dataset.data[:,0])
+                reg.fit_model()
+                print('Regression summary', reg.fitted_model.coef_)
+                print('Performing assumption checks')
+                reg.checks()
+                print('Taking any necessary corrective actions')
+                reg.mcAction()
+                reg.acAction()
+                reg.linAction()
+                reg.singAction()
+                reg.homoskeAction()
+            else:
+                print('Dataset is very large, running SGD Regression')
+                reg = sgd.SGD(dataset.data[:,1:dataset.data.shape[1]], dataset.data[:,0])
+                reg.fit_model()
+                print('Regression summary', reg.fitted_model.coef_)
+        else:
+            print('Input not recognized')
+            return dataset
 
 
 
