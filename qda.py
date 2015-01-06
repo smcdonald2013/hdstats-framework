1c45b588eb7d96500564c7b7fe07b3c55431de4d
Stephen McDonald <smcdonald2013@gmail.com>
2015-01-05 23:55:00 -0800

    QDA with initial assumption checks

---
+++ qda.py
@@ -0,0 +1,36 @@
+from sklearn.qda import QDA as skQDA
+import statsmodels.api as sm
+import checks as c
+import numpy as np
+
+class QDA:
+    #Implements quadratic discriminant analysis classification, with assumption checks
+
+    def __init__(self, data, classes):
+        self.data = data
+        self.classes = classes
+        self.classObj = skQDA()
+
+    def fit_model(self):
+        self.fitted_model = self.classObj.fit(self.data, self.classes)
+
+    def checks(self):
+        self.mvnCheck = c.mvnCheck(self.data)
+        self.mvnCheck.check()
+        self.mcCheck = c.mcCheck(self.data)
+        self.mcCheck.check()
+
+    def Actions(self):
+        self.mvnAction()
+    
+    def acAction(self):
+        if self.acCheck.ljungbox[1][0] < .05:
+            print "Residuals are autocorrelated. Implementing GLSAR."
+            return sm.regression.linear_model.GLSAR(self.dependentVar, self.independentVar)
+        else:
+            print "Residuals appear to be uncorrelated."
+
+    def singAction(self):
+        if self.singCheck == True:
+            print "Singular matrix"
+            #Remove linearly dependent rows
