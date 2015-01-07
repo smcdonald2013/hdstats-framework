e758c8093228146444308b8d8c8f7981ee813ef4
Brenhin Keller <cbkeller@princeton.edu>
2015-01-07 03:00:27 -0500

    Added Spectral Clustering class

--- clustering.py
+++ clustering.py
@@ -94,3 +94,35 @@ class MeanShift:
     def plot_results(self):
         # plot cluster centers along with all data, colored by nearest cluster
         pass
+
+
+# Class to interface with Spectral Clustering object from scikit-learn cluster module
+class SpectralClustering:
+    def __init__(self, data, n_clusters=8, eigen_solver=None, random_state=None, n_init=10, gamma=1.0, affinity='rbf', n_neighbors=10, eigen_tol=0.0, assign_labels='kmeans', degree=3, coef0=1, kernel_params=None):
+        self.n_clusters=n_clusters
+        self.eigen_solver=eigen_solver
+        self.random_state=random_state
+        self.n_init=n_init
+        self.gamma=gamma
+        self.affinity=affinity
+        self.n_neighbors=n_neighbors
+        self.eigen_tol=eigen_tol
+        self.assign_labels=assign_labels
+        self.degree=degree
+        self.coef0=coef0
+        self.kernel_params=kernel_params
+
+        self.data = data
+        self.result = None
+        self.dataTransformed = None
+        self.obj = cluster.SpectralClustering(n_clusters=self.n_clusters, eigen_solver=self.eigen_solver, random_state=self.random_state, n_init=self.n_init, gamma=self.gamma, affinity=self.affinity, n_neighbors=self.n_neighbors, eigen_tol=self.eigen_tol, assign_labels=self.assign_labels, degree=self.degree, coef0=self.coef0, kernel_params=self.kernel_params)
+
+    def fit_model(self):
+        self.result = self.obj.fit_predict(self.data) # Index of closest cluster
+
+    def print_results(self):
+        pass
+
+    def plot_results(self):
+        # plot cluster centers along with all data, colored by nearest cluster
+        pass
