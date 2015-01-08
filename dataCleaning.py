33cb185dc7cf3a12599c2e26624e828c703536fa
Brenhin Keller <cbkeller@princeton.edu>
2015-01-08 00:05:01 -0500

    Added bootstrap resampling capability to dataCleaning module

--- dataCleaning.py
+++ dataCleaning.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def main(dataset):
     while True:
-        s=raw_input('Select data cleaning method:\n  1. Remove outliers by standard deviations from the mean\n  2. Remove outliers by percentile\n  3. Replace NaNs\n  4. De-mean dataset\n  5. Normalize variance\n- 0. Exit\n')
+        s=raw_input('Select data cleaning method:\n  1. Remove outliers by standard deviations from the mean\n  2. Remove outliers by percentile\n  3. Replace NaNs\n  4. De-mean dataset\n  5. Normalize variance\n  6. Bootstrap resample dataset\n- 0. Exit\n')
         if s=='0' or s=='': # default
             break
         elif s=='1':
@@ -16,6 +16,8 @@ def main(dataset):
             dataset=demean(dataset)
         elif s=='5':
             dataset=devar(dataset)
+        elif s=='6':
+            dataset=bootstrap(dataset)
         else:
             print 'Input not recognized\n'
     return dataset
@@ -76,6 +78,35 @@ def devar(dataset):
     for i in range(dataset.data.shape[1]):
         if (np.std(dataset.data[:,i]) > 0) & (i!=dataset.independentVariable):
             dataset.data[:,i] /= np.std(dataset.data[:,i])
+    return dataset
+
+def bootstrap(dataset):
+    # Perform bootstrap sampling with replacement
+
+    # Check that error is defined, default to 5%
+    if dataset.error==None:
+        print 'Using default 5% error\n'
+        dataset.error = dataset.data * 5 / 100
+    nsamples = dataset.error.shape[0]
+    nvars = dataset.data.shape[1]
+
+    # Determine number of resamplings to perform
+    s=raw_input('Perform how many bootstrap resamplings? (default 10^6 / data length)\n')
+    try: n=int(s)
+    except: n=int(1000000 / nsamples)
+
+    # Check if data has been resampled before, cut down to size if so
+    if dataset.data.shape[0] > nsamples:
+        dataset.data = dataset.data[0:nsamples,:]
+
+    # Initialize output array
+    dataTransformed=np.zeros(shape=(n*nsamples,nvars))
+
+    # Resample
+    for i in range(n):
+        dataTransformed[(i*nsamples):((i+1)*nsamples),:] = dataset.data + dataset.error * np.random.randn(nsamples,nvars)
+
+    dataset.data = dataTransformed
     return dataset
 
 def nanmean(data):
