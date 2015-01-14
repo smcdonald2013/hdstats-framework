6cac5a032486af9f731973084650694d2afc48b4
Stephen McDonald <smcdonald2013@gmail.com>
2015-01-14 15:29:42 -0800

    Checks documentation updated.

--- checks.py
+++ checks.py
@@ -1,109 +1,153 @@
+"""@package docstring
+The checks module, which contains a variety of statistical tests.
+
+This is the location of all of the checks/tests used in the project. 
+The base check class does little beyond provide an outline that should 
+be followed when implementing checks. 
+"""
+
 from sklearn import linear_model
 import statsmodels.stats.outliers_influence as oi
 import statsmodels.api as sm
 import numpy as np
-import sys
-import scipy
-import pandas
-
+import sys, scipy, pandas
 import readline
 import pandas.rpy.common as com
 import rpy2.robjects as ro
 from rpy2.robjects.packages import importr
 
-class mcCheck:
-    #Tests for multicollinearity in the design matrix
-
-    def __init__(self, indepVar, depVar=0, residuals=0):
-        self.dependentVar = depVar
-        self.independentVar = indepVar
-        self.residuals = residuals
-
-    def check(self):
-        self.conNum = np.linalg.cond(self.independentVar)
-        if any(self.dependentVar != 0):
-            self.nVars = self.independentVar.shape[1]
-            self.vif = np.empty(self.nVars)
-            for i in range(self.nVars-1):
-                self.vif[i] = oi.variance_inflation_factor(self.independentVar,i)
-
-class acCheck:
-    #Tests for autocorrelation of the residuals using the durbin-watson test
-
-    def __init__(self, residuals):
-        self.residuals = residuals
+class check:
+    """Base check class, should be completely overwritten by child classes."""
+    
+    def __init__(self, data):
+        """Constructor, should overwrite if more than one parameter is necessary."""
+        ##Data matrix/vector, depending on the check
+        self.data = data 
 
     def check(self):
-        #self.dw = sm.stats.stattools.durbin_watson(self.residuals)
-        self.ljungbox = sm.stats.diagnostic.acorr_ljungbox(self.residuals, lags=2)
-        self.acf = sm.tsa.stattools.acf(self.residuals)
-        #PACF is behaving strangely
-        #self.pacf = sm.tsa.stattools.pacf(self.residuals)
+        """Generic check function."""
+        print('\n This should be overwritten!')
 
-class linCheck:
-    #Tests for nonlinear relationship between x's and y's. 
+class mcCheck(check):
+    """Tests for multicollinearity as measured by the condition number."""
 
+    def check(self):
+        """Finds condition number of data matrix."""
+
+        ##Condition number of matrix
+        self.conNum = np.linalg.cond(self.data)
+        if self.conNum > 20:
+            print('\nMulticollinearity is a problem')
+        else
+            print('\nThere does not appear to be an issue with multicollinearity.')
+
+class acCheck(check):
+    """Tests for autocorrelation using ljung-box test.
+
+    Generally, the input of this test will be residuals from a regression.
+    """
+    def check(self):
+        """Performs ljung-box test, by default with 2 lags."""
+        
+        ##Vector with test statistics, followed by pvalues
+        self.ljungbox = sm.stats.diagnostic.acorr_ljungbox(self.data, lags=2)
+        if self.ljungbox[1][0] < .05:
+            print('\nResiduals are autocorrelated.')
+        else
+            print('\nThere does not appear to be a problem with autocorrelation of residuals.')
+
+class linCheck(check):
+    """Tests for nonlinear relationship between dependent and independent variables. 
+
+    Note that the current implementation of this test uses the harvey-collier test, which scales poorly. 
+    """
     def __init__(self, indepVar, depVar):
+        """Creates a linCheck object.
+
+        @param indepVar Array of independent variables
+        @param depVar Vector of dependent variable
+        """
+        ## Vector of dependent variable values
         self.dependentVar = depVar
+        ## Array of independent variables
         self.independentVar = indepVar
 
     def check(self):
+        """Performs linearity check. 
+
+        This must refit the model using the statsmodels OLS method, since
+        that provides the input necessary for the harvey-collier test.
+        """
+        ## Statsmodel OLS object
         self.model = sm.OLS(self.dependentVar, self.independentVar[:,0])
+        ## Vector giving the test statistic and p-value respectively
         self.hc = sm.stats.diagnostic.linear_harvey_collier(self.model.fit())
-        #self.rain = sm.stats.diagnostic.linear_rainbow(self.model.fit())
 
-class normCheck:
-    #Tests for normality of errors. 
-
-    def __init__(self, residuals):
-        self.residuals = residuals
+class normCheck(check):
+    """Tests for normality of errors using anderson-darling test.""" 
 
     def check(self):
-        self.norm = sm.stats.diagnostic.normal_ad(self.residuals)
+        """Performs anderson-darling test."""
+        
+        ##Vector giving the test statistic and p-value respectively
+        self.norm = sm.stats.diagnostic.normal_ad(self.data)
 
-class homoskeCheck:
-    #Tests for heteroskedasticity
+class homoskeCheck(check):
+    """Tests for heteroskedasticity."""
 
     def __init__(self, residuals, indepVar):
+        """Constructs test.
+        
+        @param indepVar Array of independent variables
+        @param residuals Vector of residuals
+        """
+        ## Array of independent variables
         self.independentVar = indepVar
+        ## Vector of residuals
         self.residuals = residuals
 
     def check(self):
+        """Performs breusch-pagan test for heteroskedasticity."""
+        ##Vector containing test statistic and p-value respectively
         self.bptest = sm.stats.diagnostic.het_breushpagan(self.residuals, self.independentVar)
+        if self.bptest[1] < .05:
+            print('\nEvidence of heteroskedasticity.')
+        else:
+            print('\nHeteroskedasticity does not appear to be an issue.')
 
 class singCheck:
-    #Tests if matrix is singular, because for some unfathomable reason scikit does not throw an exception when running a regression with singular design matrix
-
-    def __init__(self,indepVar):
-        self.independentVar = indepVar
-
+    """Tests if matrix is singular."""
+    
     def check(self):
-        if np.linalg.cond(self.independentVar) < 1/sys.float_info.epsilon:
-            #Matrix is good!
+        """Uses condition number of matrix to test for exact singularity."""
+        if np.linalg.cond(self.data) < 1/sys.float_info.epsilon:
+            ## Boolean declaring singularity status of matrix
             self.singCheck = False
+            print('Data matrix is not singular.')
         else:
-            print "Matrix is singular!"
+            print('Matrix is singular!')
             self.sing = True
 
 class highdimCheck:
-    #Tests if data is high dimensional, i.e. p>n
-    
-    def __init__(self,indepVar):
-        self.independentVar = indepVar
+    """Tests if data is high dimensional, i.e. p>n."""
 
     def check(self):
-        if self.independentVar.shape[0] < self.independentVar.shape[1]:
+        """Performs test simply by examing matrix dimensions."""
+        if self.data.shape[0] < self.data.shape[1]:
             print "Data is high-dimensional!"
+            ##Boolean declaring high-dimensionality status of matrix
             self.hd = True
         else:
             self.hd = False
 
 class mvnCheck:
-    #Tests if the data come from a multivariate normal distribution
-    def __init__(self,data):
-        self.data = data
-
+    """Tests if the data come from a multivariate normal distribution using R mardia test."""
+    
     def check(self):
+        """Performs a mardia test for multivariate normality.
+
+        Implementation currently done using R.
+        """
         importr('psych')
         pan_data = pandas.DataFrame(self.data)
         r_data = com.convert_to_r_dataframe(pan_data)
@@ -111,17 +155,28 @@ class mvnCheck:
         ro.r('mardia_output = mardia(r_data, plot=FALSE)')
         ro.r('pvals = c(mardia_output$p.skew, mardia_output$p.kurtosis)')
         pan_data = com.load_data('pvals')
+        ##P-value for mardia skew test
         self.skewp = pan_data[0]
-        #self.kurp = pan_data[1] R mardia function does not currently implement kurtosis test
 
 class eqCovCheck:
-    #Tests if different classes have the same covariance matrix
+    """Tests if different classes have the same covariance matrix.
+
+    Not currently implemented due to issues with R format.
+    """
     
     def __init__(self,data,classes):
+        """Class constructor.
+        
+        @param data data matrix
+        @param classes class label vector
+        """
+        ##Data matrix
         self.data = data
+        ##Vector of class labels
         self.classes = classes
 
     def check(self):
+        """Performs check, some issue with output however."""
         importr('biotools')
         pan_data = pandas.DataFrame(self.data)
         pan_classes = pandas.DataFrame(self.classes)
@@ -132,12 +187,5 @@ class eqCovCheck:
         ro.r('boxM_test = boxM(data=r_data,grouping=r_classes)')
         ro.r('pvals = boxM_test$p.value')
         pan_data = com.load_data('pvals')
+        ##P-value of box test of equal covariances
         self.boxMp = pan_data[0]
-
-class conIndCheck:
-    #Tests if the data variables are independent, conditional on the class labels. Note that since this test is only reliable if the underlying data are gaussian, it is really a correlation test
-
-    def __init__(self,data,classes):
-        self.data = data
-        self.classes = classes
-        self.bnlearn = importr('bnlearn')
