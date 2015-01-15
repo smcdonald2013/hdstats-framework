3e6da40019d520c55a0b9b87ad5e92a5d8c04fc0
Brenhin Keller <cbkeller@princeton.edu>
2015-01-14 21:17:27 -0500

    Additions to documentation of main file

--- hdstats-framework.py
+++ hdstats-framework.py
@@ -6,10 +6,19 @@ import numpy as np
 
 ##@mainpage hdstats-framework
 #@brief Framework for import, cleaning, analysis, and visualization of high-dimensional data.
+# Installation: None
+#
+# Usage: 
+#@code{.sh}
+#    $ python hdstats-framework.py
+# @endcode
 #
 #The main user interface is a command-line based textual menu interface, with the 
 #addition of a simple built-in python interpreter to allow the user to access and 
 #interact with underlying classes and variables directly for flexibility.
+#
+#The included documentation of classes and methods is intended to allow developers
+#and others familiar with python to use and extend these methods
 #
 #The code is divided into four main files, containing UI and analysis functions: 
 #dataImport.py, dataCleaning.py, dataAnalysis.py, and dataVisualization.py
@@ -21,6 +30,7 @@ import numpy as np
 class Dataset(object):
     def __init__(self, data=None, error=None, variableNameToNumber=None, variableNumberToName=None, independentVariable=np.NaN, dependentVariable=np.NaN):
         self.data=data   ##!< Numpy array containing full dataset
+        
         ## Optional:
         self.error=error ##!< Numpy array containing errors for each data point
         self.variableNameToNumber=variableNameToNumber ##< Dictonary mapping from variable names to column numbers 
@@ -32,41 +42,50 @@ class Dataset(object):
 ## Create dataset object
 dataset=Dataset()
 
-## Set global printing options for numpy arrays
+## Set global pretty printing options for numpy arrays
 np.set_printoptions(precision=3, suppress=True)
 
 
 while True:
+    ## Main menu
     s=raw_input('Select task:\n  1. Import\n  2. Data cleaning\n  3. Analysis\n  4. Visualization\n  5. Enter Python interpreter\n  0. Exit\n')
     if s=='0':
+        ## Exit program
         break
     elif s=='1':
+        ## Enter import menu
         dataset=dataImport.main(dataset)
     elif s=='2':
+        ## Enter data cleaning menu
         dataset=dataCleaning.main(dataset)
     elif s=='3':
+        ## Enter data analysis menu
         dataset=dataAnalysis.main(dataset)
     elif s=='4':
+        ## Enter data visualization meu
         dataVisualization.main(dataset)
     elif s=='5':
+        ## Enter built-in interpreter mode
+
         print 'Type \'0\' or  \'exit\' to exit interpreter\n\n'
         while (s!='exit') & (s!='0'):
             s=raw_input('In: ')
             if s=='':
                 pass
             elif (('=' in s) and not (('==' in s) or ('!=' in s) or ('>=' in s) or ('<=' in s))) or ('import' in s):
-                ## Catch some common commands that can only be handled by exec()
+                # Catch some common commands that can only be handled by exec()
                 try:
                     exec(s)
                 except Exception, e:
                     print 'Error: %s' % e
             else:
-                ## Other statements can be evaluated, so we can print the results to screen
+                # Other statements can be evaluated, so we can print the results to screen
                 try:
                     print(eval(s))
                 except Exception, e:
                     print 'Error: %s' % e
     else:
+        ## Return to main menu in case of unrecognized input
         print 'Input not recognized\n'
 
 
